2aa15b93c93affba6c5e5be6705f4f7e7574baa1
Dhananjaylodhi <54372914+Dhananjaylodhi@users.noreply.github.com>
2020-05-15 16:24:20 +0530

    Delete program4.py

--- program4.py
+++
@@ -1,6 +0,0 @@
-cost_price = int(input("enter cost price: "))
-selling_price = int(input("enter selling price: "))
-profit = selling_price - cost_price
-print("The profit is: ", profit)
-increment = 1.05*profit + cost_price
-print("price after 5% profit increment: ", increment)
